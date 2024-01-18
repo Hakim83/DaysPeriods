d275703a3e3fa4aaa0173b04681660e6814bbdc8
Mohammed Abdalhakam <mabdalhakam31431@cudd.com>
2024-01-18 16:54:38 +0200

    initial commit

---
+++ src/types/TimesData.ts
@@ -0,0 +1,11 @@
+
+export class TimesData {
+    date: Date;
+    sunrise: Date;
+    sunset: Date;
+    constructor(date: Date, sunrise: Date, sunset: Date) {
+        this.date = date;
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+    }
+}
